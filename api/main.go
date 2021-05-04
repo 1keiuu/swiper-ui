7077bad8179acf5416f30fb7ae350e1fd83d48f3
1keiuu <ikkei12.h@gmail.com>
2021-05-04 14:16:23 +0900

    goでhotreload出来るように

--- api/main.go
+++ api/main.go
@@ -6,18 +6,29 @@ import (
 	"net/http"
 )
 
-type Data struct {
-	Name   string
-	imgURL string
-	age    int
+type User struct {
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	ImgURL string `json:"imageURL"`
+	Age    int    `json:"age"`
 }
 
 func handler(res http.ResponseWriter, req *http.Request) {
-	data := Data{Name: "", imgURL: "https://placekitten.com/g/200/300", age: 20}
+	users := []struct {
+		Id     int    `json:"id"`
+		Name   string `json:"name"`
+		ImgURL string `json:"imageURL"`
+		Age    int    `json:"age"`
+	}{
+		{Id: 1, Name: "test1", ImgURL: "https://placekitten.com/g/200/300", Age: 20},
+		{Id: 2, Name: "test2", ImgURL: "https://placekitten.com/g/200/300", Age: 22},
+		{Id: 3, Name: "test3", ImgURL: "https://placekitten.com/g/200/300", Age: 27},
+		{Id: 4, Name: "test4", ImgURL: "https://placekitten.com/g/200/300", Age: 35},
+	}
 	res.Header().Set("Access-Control-Allow-Headers", "*")
 	res.Header().Set("Access-Control-Allow-Origin", "*")
 	res.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	output, _ := json.MarshalIndent(&data, "", "\t\t")
+	output, _ := json.MarshalIndent(&users, "", "\t\t")
 	res.Write(output)
 }
 
