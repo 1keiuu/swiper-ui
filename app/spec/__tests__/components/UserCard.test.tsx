f71c73df9085104178d7b0a434bea080c936e452
1keiuu <ikkei12.h@gmail.com>
2021-05-10 13:32:42 +0900

    UserCardのtest追加

--- app/spec/__tests__/components/UserCard.test.tsx
+++ app/spec/__tests__/components/UserCard.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, render, fireEvent, waitFor } from "@testing-library/react";
 
 import UserCard from "../../../src/components/user-card/UserCard";
+import * as CurrentUserCardContext from "../../../src/context/CurrentUserCardContext";
 
-describe("UserCard Component", () => {
+describe("UserCard Component(UI)", () => {
   const valideUser = {
     id: 1,
     name: "test1",
@@ -20,7 +21,7 @@ describe("UserCard Component", () => {
     );
     expect(asFragment()).toMatchSnapshot();
   });
-  test("render front UI correctly.", () => {
+  test("render card's front correctly.", () => {
     const { asFragment } = render(
       <UserCard user={valideUser} index={1} currentCardIndex={1}></UserCard>
     );
@@ -40,7 +41,7 @@ describe("UserCard Component", () => {
     expect(userCardName.textContent).toBe(`${valideUser.name},`);
     expect(userCarAge.textContent).toBe(String(valideUser.age));
   });
-  test("render back UI correctly.", () => {
+  test("render card's back correctly.", () => {
     const { asFragment } = render(
       <UserCard user={valideUser} index={1} currentCardIndex={1}></UserCard>
     );
@@ -64,5 +65,48 @@ describe("UserCard Component", () => {
     expect(
       (prev.container.firstChild as HTMLDivElement).classList.contains("--next")
     );
+  });
+});
+
+describe("UserCard Component(event/context)", () => {
+  // カードクリック時,スワイプ時にcontext(dispatch)が正常に呼び出されているかのテスト
+  let useCurrentUserCardDispatchSpy = jest.spyOn(
+    CurrentUserCardContext,
+    "useCurrentUserCardDispatch"
+  );
+  const toggleIsFlippedDispatchSpy = jest.fn();
+  const setIsFlippedDispatchSpy = jest.fn();
+  const incrementIndexDispatchSpy = jest.fn();
+  const decrementIndexDispatchSpy = jest.fn();
+  const changeStatusDispatchSpy = jest.fn();
+
+  useCurrentUserCardDispatchSpy.mockImplementation(() => ({
+    toggleIsFlipped: toggleIsFlippedDispatchSpy,
+    setIsFlipped: setIsFlippedDispatchSpy,
+    incrementIndex: incrementIndexDispatchSpy,
+    decrementIndex: decrementIndexDispatchSpy,
+    changeStatus: changeStatusDispatchSpy,
+  }));
+  const valideUser = {
+    id: 1,
+    name: "test1",
+    imgURL: "http://localhost/image",
+    age: 21,
+    profile: "test1's profile",
+  };
+  test("dispatch 'toggleIsFlipped' when it's clicked", async () => {
+    const card = render(
+      <UserCard user={valideUser} index={1} currentCardIndex={0}></UserCard>
+    ).container.firstChild;
+    const inner = card.firstChild;
+    expect((inner as HTMLDivElement).classList.contains("--flipped")).toBe(
+      false
+    );
+    fireEvent.click(card);
+    expect(toggleIsFlippedDispatchSpy).toBeCalled();
+  });
+  test("fire correct events when it's swiped", () => {
+    // NOTE: swipe eventのテストの追加
+    // スワイプ量の取得の必要があるので、Enzymeとか使う必要がありそう？
   });
 });
